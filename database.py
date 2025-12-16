dab6b755c083d70bec99283eaa130c1071c370ff
Tanay <tanayj2107@gmail.com>
2025-12-16 18:41:58 -0500

    created articles table

--- database.py
+++ database.py
@@ -2,3 +2,20 @@ import sqlite3
 
 conn = sqlite3.connect('news.sentiment.db')
 
+#Create a cursor
+c = conn.cursor()
+
+#Create an article table
+# language=sql
+c.execute("""CREATE TABLE articles (
+    article_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    ticker TEXT,
+    title TEXT,
+    description TEXT,
+    content TEXT,
+    source TEXT,
+    published_at TEXT,
+    url TEXT UNIQUE,
+    author TEXT
+)""")
+conn.commit()
