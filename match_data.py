53aaa077c608bac4e9e0fe4ca14169e7051ddf7b
Tanay <tanayj2107@gmail.com>
2025-12-19 16:32:17 -0500

    saved data to csv

--- match_data.py
+++ match_data.py
@@ -71,6 +71,8 @@ print(daily_stats.head())
 
 daily_stats['next_day_change'] = (daily_stats.groupby('ticker')['close_price'].shift(-1)- daily_stats['close_price']) / daily_stats['close_price'] * 100
 
+daily_stats.to_csv('daily_stats.csv')
+
 conn.commit()
 
 conn.close()
